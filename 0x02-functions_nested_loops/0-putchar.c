d2fdb44aed7a0597ac57f930555a21ccdfd86742
SurafelAbera <93444317+Grey-Surafel-Abera@users.noreply.github.com>
2022-04-12 19:51:52 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
+
+  
 #include "main.h"
 
 /**
- * prints _putchar() - Prints the "_putchar().
+ * main - Prints _putchar.
  *
  * Return: Always 0 (Success)
  */
-void print_char(void)
+int main(void)
 {
-		_putchar("_putchar");
-                _putchar('\n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114 };
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
 	}
+	_putchar('\n');
+	return (0);
+}
