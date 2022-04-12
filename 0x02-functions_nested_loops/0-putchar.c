aa7e5a9d0e661a2768f235ef624dc3b7fdc350fa
SurafelAbera <93444317+Grey-Surafel-Abera@users.noreply.github.com>
2022-04-12 18:50:48 +0000

    Update 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * prints _putchar() - Prints the "_putchar().
+ *
+ * Return: Always 0 (Success)
+ */
+void print_char(void)
+{
+		_putchar("_putchar");
+                _putchar('\n');
+	}
